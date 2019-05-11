c4baf89346ac3f2a2e7f26cdb57f3532a466bbf1
Lyly <yhl4340@gmail.com>
2019-05-11 10:08:23 -0700

    Updates

---
+++ precache-manifest.e1e8ee3ec9d2a802e585618b6e578c0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0632708f1e2fe9a74aec89d4c5160a9",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "67f2677fe72ff1b3939f",
+    "url": "/clicky-game/static/css/main.1aaed2e4.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "67f2677fe72ff1b3939f",
+    "url": "/clicky-game/static/js/main.8fd70f21.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
